6b54e7f9ce6de4ca9a88c288152219f95960c474
cass430 <156031807+cass430@users.noreply.github.com>
2024-11-20 18:34:13 -0700

    updates

--- assets/main.js
+++ assets/main.js
@@ -9,3 +9,35 @@ document
       menu.style.display = "flex";
     }
   });
+
+// sliding effects on home page
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Select all elements with either hidden-left or hidden-right class
+  const rows = document.querySelectorAll(".hidden-left, .hidden-right");
+
+  // Intersection Observer to reveal elements on scroll
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show"); // Add the show class
+          observer.unobserve(entry.target); // Stop observing once shown
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  // Observe each row with hidden-left or hidden-right class
+  rows.forEach((row) => observer.observe(row));
+});
+
+//Typewritter Effect
+
+new Typewriter("#my-name", {
+  strings: ["Cassidy Perry"],
+  autoStart: true,
+  cursor: null,
+  loop: true,
+});
